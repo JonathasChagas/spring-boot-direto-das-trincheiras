a1c9069f9f13820f6cec505956e570a16afb6527
JonathasChagas <jonathas252423@gmail.com>
2024-10-24 11:31:41 -0300

    video18

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +15,12 @@ public class Producer {
     private Long id;
     @JsonProperty("name")
     private String name;
+    private LocalDateTime createdAt;
 
     @Getter
     private static List<Producer> producers = new ArrayList<>(List.of(
-            Producer.builder().name("Mappa").id(1L).build(),
-            Producer.builder().name("Kyoto Animation").id(2L).build(),
-            Producer.builder().name("Madhouse").id(3L).build()
+            Producer.builder().name("Mappa").id(1L).createdAt(LocalDateTime.now()).build(),
+            Producer.builder().name("Kyoto Animation").id(2L).createdAt(LocalDateTime.now()).build(),
+            Producer.builder().name("Madhouse").id(3L).createdAt(LocalDateTime.now()).build()
     ));
 }
