0345f5b919d996e34bc7d006dde3b5d4aef9b9ac
JonathasChagas <jonathas252423@gmail.com>
2024-10-27 12:27:21 -0300

    video24

--- anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
+++ anime-service/src/main/java/academy/devdojo/mapper/ProducerMapper.java
@@ -3,13 +3,14 @@ package academy.devdojo.mapper;
 import academy.devdojo.domain.Producer;
 import academy.devdojo.domain.Producer;
 import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.request.ProducerPutRequest;
 import academy.devdojo.response.ProducerPostResponse;
 import academy.devdojo.response.ProducerGetResponse;
 import academy.devdojo.response.ProducerPostResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -19,6 +20,7 @@ public interface ProducerMapper {
     @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "id", expression = "java(academy.devdojo.domain.Producer.getProducers().stream().mapToLong(Producer::getId).max().orElseThrow(java.util.NoSuchElementException::new) + 1)")
     Producer toProducer(ProducerPostRequest postRequest);
+    Producer toProducer(ProducerPutRequest putRequest, LocalDateTime createdAt);
 
     ProducerPostResponse toProducerPostResponse(Producer producer);
 
